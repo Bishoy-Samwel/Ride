c9164f2c5005bc24ff2602b94834fa29b7626478
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-05-24 11:17:40 +0200

    fix(admin panel): permit styles params

--- app/admin/styles.rb
+++ app/admin/styles.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Style do
-
+  permit_params :name, bike_ids:[]
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
