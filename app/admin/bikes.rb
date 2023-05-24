886a8a02a71888c05bc667fb96f805cd0ae296b1
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-05-24 11:22:21 +0200

    feat(admin panel): admin can upload bike image

--- app/admin/bikes.rb
+++ app/admin/bikes.rb
@@ -3,15 +3,26 @@ ActiveAdmin.register Bike do
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
-  #
+
   permit_params :name, :desc, :price, :style_id, :image
-  #
+
   # or
-  #
+
   # permit_params do
   #   permitted = [:name, :desc, :price, :style_id]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :desc
+      f.input :price
+      f.input :style
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 
 end
