14370c114b1039db43b94a3d4f34c7660725e0bd
Bishoy Samwel <bishoy.samwuel@gmail.com>
2023-05-26 09:32:39 +0200

    fix(telegram bot):run telegram bot inside a thread

--- config/initializers/telegram_bot.rb
+++ config/initializers/telegram_bot.rb
@@ -32,25 +32,28 @@ def format_bikes(text)
   # result.map{|name, price| "bike #{name} will cost #{price}"}.join( "\n\n")
 end
 
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id,
-                           text: "Hello, #{message.from.first_name}")
-    when '/browse_bikes'
-      bot.api.send_message(chat_id: message.chat.id,
-                           text: 'You can filter bikes:',
-                           reply_markup: keyboard(REPLIES.keys))
-    when '/search'
-      bot.api.send_message(chat_id: message.chat.id,
-                           text: 'Type the name of the bike!')
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id,
-                           text: "Bye, #{message.from.first_name}")
-    else
-      bot.api.send_message(chat_id: message.chat.id,
-                           text: format_bikes(message.text))
+Thread.new do
+  Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+      case message.text
+      when '/start'
+        bot.api.send_message(chat_id: message.chat.id,
+                             text: "Hello, #{message.from.first_name}")
+      when '/browse_bikes'
+        bot.api.send_message(chat_id: message.chat.id,
+                             text: 'You can filter bikes:',
+                             reply_markup: keyboard(REPLIES.keys))
+      when '/search'
+        bot.api.send_message(chat_id: message.chat.id,
+                             text: 'Type the name of the bike!')
+      when '/stop'
+        bot.api.send_message(chat_id: message.chat.id,
+                             text: "Bye, #{message.from.first_name}")
+      else
+        bot.api.send_message(chat_id: message.chat.id,
+                             text: format_bikes(message.text))
+      end
     end
   end
 end
+
